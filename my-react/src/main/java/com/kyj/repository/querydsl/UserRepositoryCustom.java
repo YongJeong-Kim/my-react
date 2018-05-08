7871e3cf75272ce315ac61512112ead34cff495f
yongjeong-kim <hushigine@gmail.com>
2018-05-08 16:30:40 +0900

    180508

--- my-react/src/main/java/com/kyj/repository/querydsl/UserRepositoryCustom.java
+++ my-react/src/main/java/com/kyj/repository/querydsl/UserRepositoryCustom.java
@@ -1,14 +1,9 @@
 package com.kyj.repository.querydsl;
 
-import org.springframework.transaction.annotation.Transactional;
-
 import com.kyj.dto.ProfileDTO;
 import com.kyj.dto.UserDTO;
 
 public interface UserRepositoryCustom {
-	@Transactional
-	public void setUserProfile(ProfileDTO profileDTO, String username);
-	
-	@Transactional
-	public void setUserCard(UserDTO userDTO, String username);
+	void setUserProfile(ProfileDTO profileDTO, String username);
+	void setUserCard(UserDTO userDTO, String username);
 }
